275689b33ee40fd7e9e8317ae0bc8a004f168d57
Andrew Venson <vensoneandrew@gmail.com>
2022-09-13 00:44:20 -0500

    using key code to quit

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h> /* printf and fprintf */
-#include <stdbool.h>
 
 #ifdef _WIN32
 #include <SDL/SDL.h> /* Windows-specific SDL2 library */
@@ -12,64 +11,76 @@
 #define HEIGHT 600
 #define DELAY 3000
 
-void quitGame(SDL_Window *window){
-  /* Pauses all SDL subsystems for a variable amount of milliseconds */
-  SDL_Delay(DELAY);
+void quitGame(SDL_Window *window, int *game){
+    printf("This the game right here %d", game);
 
-  /* Frees memory */
-  SDL_DestroyWindow(window);
-  
-  /* Shuts down all SDL subsystems */
-  SDL_Quit(); 
+    *game = 0;
+    /* Pauses all SDL subsystems for a variable amount of milliseconds */
+    //SDL_Delay(DELAY);
+
+    /* Frees memory */
+    SDL_DestroyWindow(window);
+
+    /* Shuts down all SDL subsystems */
+    SDL_Quit(); 
+
 }
 
 int main (int argc, char **argv)
 {
-  /* Initialises data */
-  SDL_Window *window = NULL;
-  SDL_Event event;
-  bool game = false;
-  
-  /*
-  * Initialises the SDL video subsystem (as well as the events subsystem).
-  * Returns 0 on success or a negative error code on failure using SDL_GetError().
-  */
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    fprintf(stderr, "SDL failed to initialise: %s\n", SDL_GetError());
+
+    const int QUIT_CODE = 113;
+    /* Initialises data */
+    SDL_Window *window = NULL;
+    SDL_Event event;
+
+    int game = 0;
+    int *pGame;
+    pGame = &game;
+
+    /*
+    * Initialises the SDL video subsystem (as well as the events subsystem).
+    * Returns 0 on success or a negative error code on failure using SDL_GetError().
+    */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SDL failed to initialise: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    /* Creates a SDL window */
+    window = SDL_CreateWindow("New World", /* Title of the SDL window */
+                SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
+                SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
+                WIDTH, /* Width of the window in pixels */
+                HEIGHT, /* Height of the window in pixels */
+                0); /* Additional flag(s) */
+
+    /* Checks if window has been created; if not, exits program */
+    if (window == NULL) {
+        fprintf(stderr, "SDL window failed to initialise: %s\n", SDL_GetError());
     return 1;
-  }
+    }else{
+        game = 1;
+    }
 
-  /* Creates a SDL window */
-  window = SDL_CreateWindow("New World", /* Title of the SDL window */
-			    SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
-			    SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
-			    WIDTH, /* Width of the window in pixels */
-			    HEIGHT, /* Height of the window in pixels */
-			    0); /* Additional flag(s) */
+    while(game==1){
+        if(SDL_PollEvent(&event)){
+            switch(event.type){
+                    case SDL_KEYDOWN:
+                        printf("Key down detected: %d\n", event.key.keysym.sym);
+                        break;
+                    case SDL_KEYUP:
+                        printf("Key up detected: %d\n", event.key.keysym.sym);
+                        if(event.key.keysym.sym == QUIT_CODE){
+                            quitGame(window, pGame);
+                        }
+                        break;
+                    default:
+                        break;
+            }
+        }
+    }
 
-  /* Checks if window has been created; if not, exits program */
-  if (window == NULL) {
-    fprintf(stderr, "SDL window failed to initialise: %s\n", SDL_GetError());
-    return 1;
-  }else{
-      game = true;
-  }
-
-  while(game){
-      while(SDL_PollEvent(&event)){
-          switch(event.type){
-              case SDL_KEYDOWN:
-                  printf("Key down detected");
-                  break;
-                case SDL_KEYUP:
-                  printf("Key up detected");
-                  quitGame(window, game);
-                  break;
-                default:
-                  break;
-          }
-      }
-  }
-  
+  printf("World ended\n");
   return 0;
 }
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gcc main.c -o sdl -lSDL2 -std=c99
+./sdl
